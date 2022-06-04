c302eb0975b16eae4ec83f44a4e5eb0219875563
sudo.park <sudo.park@kakao.com>
2022-06-05 01:21:18 +0900

    노션 페이지 리다이렉트 하는 방식으로 변경

--- src/components/welcomes/Welcome.js
+++ src/components/welcomes/Welcome.js
@@ -1,19 +1,19 @@
 
 
 import React from "react";
-import page0 from "../../markdowns/welcome-index.md"
-import page1 from "../../markdowns/welcome-add-item.md"
-import MarkdownViewer from "../common/MarkdownViewer";
 
 class Welcome extends React.Component {
 
+
+    componentDidMount() {
+        window.location.href = "https://readmind.notion.site/1-e7892671b2dd4a27b58a1d8b015088ac"
+    }
+
     render() {
         return (
-            <>
-            <MarkdownViewer sourcePath={page0}/>
-            <div>------------</div>
-            <MarkdownViewer sourcePath={page1}/>
-            </>
+            <div>
+                Redirect to app start guide page..
+            </div>
         );
     }
 }
