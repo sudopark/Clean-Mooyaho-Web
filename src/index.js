214bac8a114a17d935489f4c4c972c036a196572
sudo.park <sudo.park@kakao.com>
2022-06-06 18:04:31 +0900

    welcome 페이지 다국어 라우팅 분기

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import AppRouter from './components/AppRouter';
 import reportWebVitals from './reportWebVitals';
+import './i18n'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
