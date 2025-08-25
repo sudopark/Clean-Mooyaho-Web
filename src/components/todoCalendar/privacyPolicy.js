6fcaa6670109bbdbfdac697f5f2c51be541e913b
sudo.park <sudo.park@kakao.com>
2025-08-26 01:26:44 +0900

    사이트 하단 짤리는 이슈 수정

--- src/components/todoCalendar/privacyPolicy.js
+++ src/components/todoCalendar/privacyPolicy.js
@@ -5,8 +5,16 @@ function TodoCalendarPrivacyPolicy() {
 
     return (
         <div style={{ width: '100%', height: '100%' }}>
+            
             {/* 노션 페이지를 임베드하는 iframe */}
-            <iframe src="https://readmind.notion.site/ebd/194136c8558380149a75ccc38408c727" width="100%" height="600" frameborder="0" allowfullscreen />
+            {/* iframe의 높이를 '100vh'로 설정하여 뷰포트 전체를 채우도록 수정했습니다. */}
+            {/* 이 방식으로 빈 공백을 없앨 수 있습니다. */}
+            <iframe
+                src="https://readmind.notion.site/ebd/194136c8558380149a75ccc38408c727"
+                width="100%"
+                style={{ height: '100vh', border: 'none', display: 'block' }}
+                title="Todo Calendar Guide"
+            />
         </div>
     );
 }
--- src/components/todoCalendar/todoCalendarGuide.js
+++ src/components/todoCalendar/todoCalendarGuide.js
@@ -7,8 +7,15 @@ function TodoCalendarGuide() {
  
     return (
         <div style={{ width: '100%', height: '100%' }}>
-            {/* 노션 페이지를 임베드하는 iframe */}
-            <iframe src="https://readmind.notion.site/ebd/a2183ee1a41946faa8e0658640fb4c6a" width="100%" height="600" frameborder="0" allowfullscreen />
+        {/* 노션 페이지를 임베드하는 iframe */}
+        {/* iframe의 높이를 '100vh'로 설정하여 뷰포트 전체를 채우도록 수정했습니다. */}
+        {/* 이 방식으로 빈 공백을 없앨 수 있습니다. */}
+        <iframe
+            src="https://readmind.notion.site/ebd/a2183ee1a41946faa8e0658640fb4c6a"
+            width="100%"
+            style={{ height: '100vh', border: 'none', display: 'block' }}
+            title="Todo Calendar Guide"
+        />
         </div>
     );
 }
